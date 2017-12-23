e7830e7ba7dc429c539fc6b34ea0c53b1764a886
pavelrosh <pavelrosh@gmail.com>
2017-12-23 17:33:51 +0200

    ft_after doesn't return str && line

--- get_next_line.c
+++ get_next_line.c
@@ -51,22 +51,24 @@ char	*ft_after(char *line, char *str, size_t n)
 	int k;
 
 	k = 0;
+//	printf("ft_after STR: %s\n", str);
 	if ((k = ft_end_line_find(str, '\n', n)) > 0)
 	{
-		printf("%s\n", "ft_end_line_find");
+		//printf("%s\n", "ft_end_line_find");
 		ft_strncat(line, str, k);
 		line[--k] = '\0';
 		str = ft_memchr(str, '\n', n);
-		return (line);
+	//	printf("IN AFTER line: %s\n", line);
+	//	printf("IN AFTER str :%s\n", str);
 	}
 	else
 	{
 		ft_strncat(line, str, n);
-	//	printf("ft_after - else : %s", line);
 		str = NULL;
-	//	printf("%s\n", "ft_strdel");
+	//	printf("ft_after - else : %s\n", line);
+	//	printf("%s\n", str);
 	}
-	return(NULL);
+	return (str);
 }
 
 int		get_next_line(const int fd, char **line)
@@ -77,51 +79,42 @@ int		get_next_line(const int fd, char **line)
 	char *tmp;
 
 	tmp = ft_strnew(BUFF_SIZE);
-	*line = ft_strnew(BUFF_SIZE);
-	if (str == NULL)
+	//printf("STR :%s\n", str);
+	//printf("line - %s\n", *line);
+	if (str)
 	{
-	//	printf("%s\n", "STR == NULL");
-		//str = ft_strnew(BUFF_SIZE);
+		str = ft_after(*line, str, BUFF_SIZE);
+		ft_alloc(*line);
+	//	printf("LINE after ft :%s\n", *line);
 	}
 	else
 	{
-	//	printf("%s\n", "else");
-		printf("STR IN ELSE = %s\n", str);
-		ft_after(*line, str, BUFF_SIZE);
-	//	printf("%s\n", "ft_after");
-		ft_alloc(*line);
-	}
-	while ((i = read(fd, tmp, BUFF_SIZE) > 0))
-	{
-	//	printf("READ = %s\n", tmp);
-	//	printf("%s\n", "INWHILE");
-		if ((k = ft_end_line_find(tmp, '\n', BUFF_SIZE)) > 0)
+		*line = ft_strnew(BUFF_SIZE);
+		while ((i = read(fd, tmp, BUFF_SIZE) > 0))
 		{
-	//		printf("%s\n","IF");
-			ft_strncat(*line, tmp, k);
-			line[0][ft_strlen(*line) - 1] = '\0'; //*line[--k] ???
-			//printf("final line :%s", *line);
-			if (k != BUFF_SIZE)
+			// printf("READ = %s\n", tmp);
+			// printf("%s\n", "INWHILE");
+			if ((k = ft_end_line_find(tmp, '\n', BUFF_SIZE)) > 0)
 			{
+				ft_strncat(*line, tmp, k);
+				line[0][ft_strlen(*line) - 1] = '\0'; //*line[--k] ???
+			//printf("final line :%s", *line);
 				str = ft_memchr(tmp, '\n', BUFF_SIZE);
-			//	printf("STR = %s\n", str);
+	//				printf("STR = %s\n", str);
+				return (1);
 			}
-			ft_strdel(&tmp);
-			return (1);
+			else
+			{
+			//	printf("%s\n", "ELSE");
+				ft_strncat(*line, tmp, BUFF_SIZE);
+			//	printf("%s\n", *line);
+			}
+			ft_alloc(*line);
 		}
-		else
-		{
-		//	printf("%s\n", "ELSE");
-			ft_strncat(*line, tmp, BUFF_SIZE);
-		//	printf("%s\n", *line);
-		}
-		ft_alloc(*line);
+		return (0);
 	}
-	return (0);
+	return (1);
 }
-
-
-
 
 
 
--- main.c
+++ main.c
@@ -16,14 +16,18 @@ int		main(int argc, char **argv)
 {
 	char *line;
 	int fd;
+	int i;
+
+	i = 0;
 	fd = open(argv[1], O_RDONLY);
 //	printf("fd = %d\n", fd);
 //	printf("%d\n", get_next_line(fd, &(*line)));
 //	get_next_line(fd, &line);
 //	printf("%s", line);
 	while ((get_next_line(fd, &line) > 0))
+	{
 		printf("%s\n", line);
+	}
 	close(fd);
 	return (0);
-
 }
